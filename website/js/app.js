51cb329d51ba68c87cb31b7a3cb004c4af614a3c
Ryan Geddes <rgeddes09@gmail.com>
2020-06-30 20:37:26 -0700

    got server working

--- website/js/app.js
+++ website/js/app.js
@@ -1,22 +1,26 @@
 'use strict';
 
 function renderThings(list) {
-  const template = $('#thingsToDoTemplate').html;
-  const container = $('.things');
+  const template = $('#thingsToDoTemplate').html();
+  const container = $('#things');
   list.forEach(item => {
-    let newItemHTML = Mustache.render(item, template)
+    let newItemHTML = Mustache.render(template, item);
     container.append(newItemHTML);
   });
 }
 
 function showThingsToDo() {
-
-  let things = [
-    { thing: 'watch tv' },
-    { thing: 'take a nap' },
-  ];
-  renderThings(things);
-
+  
+  let things = [];
+  let source = 'http://localhost:3000/todo';
+  $.ajax(source)
+    .then(data =>{
+      data.forEach(item =>{
+        things.push(item);
+      });      
+      renderThings(things);
+    });
+  
 }
 
 $(document).ready(function () {
